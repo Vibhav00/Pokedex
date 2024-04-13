2444f5e84d99146e72d52cb800b5e3a6328e0756
Vibhav <vibhavkumarmishra00170@gmail.com>
2024-04-14 00:22:02 +0530

    starst

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -66,4 +68,55 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+
+
+
+
+    kapt ("androidx.hilt:hilt-compiler:1.0.0")
+
+
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    // Timber
+    implementation ("com.jakewharton.timber:timber:4.7.1")
+
+
+
+
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0-beta01")
+    implementation ("androidx.navigation:navigation-compose:2.4.0-alpha09")
+//    implementation ("androidx.compose.material:material-icons-extended:$compose_version")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
+
+    // Coroutines
+
+
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1")
+
+    // Coroutine Lifecycle Scopes
+
+
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+
+
+    // Coil
+//    implementation "io.coil-kt:coil:1.1.1"
+
+
+    implementation ("io.coil-kt:coil-compose:2.0.0")
+    implementation ("com.google.accompanist:accompanist-coil:0.10.0")
+
+    //noinspection GradleCompatible
+//    implementation ("com.android.support:palette-v7:28.0.0")
+
 }
